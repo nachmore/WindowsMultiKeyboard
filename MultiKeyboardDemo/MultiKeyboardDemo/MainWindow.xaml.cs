725537b61dd86e29bdec1ceea15e966cf213a28c
Oren Nachman <code@nachmore.com>
2018-06-18 00:31:40 +1000

    Initial checkin (working). Demo just lists inputs in a basic table as they are typed.

---
+++ MultiKeyboardDemo/MultiKeyboardDemo/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using MultiKeyboardLib;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiKeyboardDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MultiKeyboardInput multiKeyboardInput;
+
+        private ObservableCollection<RawKeyboard> _rawKeyboard = new ObservableCollection<RawKeyboard>();
+        public ObservableCollection<RawKeyboard> Keyboards
+        {
+            get { return _rawKeyboard; }
+        }
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (multiKeyboardInput == null)
+            {
+                multiKeyboardInput = new MultiKeyboardInput(HwndSource.FromHwnd(new WindowInteropHelper(this).Handle), OnRawInput);
+            }
+        }
+
+        // TODO: You'd probably want to convert this into regular Key etc
+        private void OnRawInput(IntPtr hDevice, Win32.VK key)
+        {
+            // TODO: You really want to use an ObservableDictionary instead of this fun
+            var found = false;
+
+            foreach (var item in Keyboards)
+            {
+                if (item.Device == hDevice.ToString())
+                {
+                    found = true;
+
+                    item.Key = key.ToString();
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                Keyboards.Add(new RawKeyboard() { Device = hDevice.ToString(), Key = key.ToString() });
+            }
+        }
+    }
+
+    // TODO: You really want to use an ObservableDictionary instead of this fun
+    public class RawKeyboard : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _device;
+        public string Device
+        {
+            get { return _device; }
+            set {
+                _device = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _key;
+        public string Key
+        {
+            get { return _key; }
+            set
+            {
+                _key = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MultiKeyboardDemo/MultiKeyboardLib/MultiKeyboardInput.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using static MultiKeyboardLib.Win32;
+
+namespace MultiKeyboardLib
+{
+    public class MultiKeyboardInput
+    {
+        private HwndSource _source;
+        private Action<IntPtr, Win32.VK> _callbackRawInput;
+
+        public MultiKeyboardInput(HwndSource source, Action<IntPtr, Win32.VK> callbackRawInput)
+        {
+            _source = source;
+            _callbackRawInput = callbackRawInput;
+
+            RegisterForRawInputDevices();    
+
+            // add a window WndPro hook
+            source.AddHook(new HwndSourceHook(WndProc));
+        }
+
+        private void RegisterForRawInputDevices()
+        {
+            // register for raw input
+            RAWINPUTDEVICE[] dev = new RAWINPUTDEVICE[1];
+
+            dev[0].UsagePage = HIDUsagePage.Generic;
+            dev[0].Usage = HIDUsage.Keyboard;
+            dev[0].Flags = 0;
+            dev[0].WindowHandle = _source.Handle;
+
+            var rv = RegisterRawInputDevices(dev, 1, Marshal.SizeOf(typeof(RAWINPUTDEVICE)));
+        }
+
+        ~MultiKeyboardInput()
+        {
+            // deregister (Flags == .Remove)
+            RAWINPUTDEVICE[] dev = new RAWINPUTDEVICE[1];
+
+            dev[0].UsagePage = HIDUsagePage.Generic;
+            dev[0].Usage = HIDUsage.Keyboard;
+            dev[0].Flags = RawInputDeviceFlags.Remove;
+            dev[0].WindowHandle = _source.Handle;
+
+            RegisterRawInputDevices(dev, 1, Marshal.SizeOf(typeof(RAWINPUTDEVICE)));
+        }
+
+        public IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+
+            if (msg == (int)WM.INPUT)
+            {
+                OnInput(hwnd, wParam, lParam);
+
+                return IntPtr.Zero;
+            }
+
+            // just in case you want to do other things...
+
+            return IntPtr.Zero;
+
+        }
+
+        void OnInput(IntPtr hwnd, IntPtr code, IntPtr hRawInput)
+        {
+            uint dwSize = 0;
+
+            GetRawInputData(hRawInput, (uint)RID.Input, IntPtr.Zero, ref dwSize, (uint)Marshal.SizeOf(typeof(RAWINPUTHEADER)));
+
+            IntPtr buffer = Marshal.AllocHGlobal((int)dwSize);
+
+            if (GetRawInputData(hRawInput, (uint)RID.Input, buffer, ref dwSize, (uint)Marshal.SizeOf(typeof(RAWINPUTHEADER))) == dwSize)
+            {
+                RAWINPUT raw = (RAWINPUT)Marshal.PtrToStructure(buffer, typeof(RAWINPUT));
+
+                Win32.VK vk = (Win32.VK)raw.keyboard.VKey;
+
+                Console.WriteLine("RAWINPUT.header->hDevice: " + raw.header.hDevice + " key: " + raw.keyboard.VKey + " (" + vk.ToString() + ")");
+
+                _callbackRawInput(raw.header.hDevice, (Win32.VK)raw.keyboard.VKey);
+            }
+        }
+    }
+}
